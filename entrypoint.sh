52e24ca37fbd4a3a2753c59cc773d46aca5585e5
Anastasia <anastasiya.e.kuzmenko@gmail.com>
2025-05-17 13:43:13 +0300

    add entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "🛠 Running database migrations..."
+#python3 /app/manage.py makemigrations
+python3 /app/manage.py migrate
+
+echo "🚀 Starting server..."
+exec python3 /app/manage.py runserver 0.0.0.0:8000
