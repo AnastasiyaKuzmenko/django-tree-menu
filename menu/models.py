a4da544b3680338b7e5cce0a21aff5a68f688ab3
Anastasia <anastasiya.e.kuzmenko@gmail.com>
2025-05-17 21:07:14 +0300

    add Menu and MenuItem class

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.urls import reverse, NoReverseMatch
 
 # Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+       return self.name
+
+class MenuItem(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='items', null=True, blank=True)
+    name = models.CharField('Name', max_length=100)
+    url = models.URLField(max_length=200, null=True, blank=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    named_url = models.CharField(max_length=100, blank=True, help_text='Name of the URL pattern defined in urls.py')
+
+    def get_absolute_url(self):
+        if self.named_url:
+            try:
+                return reverse(self.named_url)
+            except NoReverseMatch:
+                return '#'
+        return self.url or '#'
+
+    def __str__(self):
+       return self.name
