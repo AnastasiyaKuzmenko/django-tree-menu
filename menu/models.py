1303c63795c70e7bf782228da3763fa99fb98b2e
Anastasia <anastasiya.e.kuzmenko@gmail.com>
2025-05-21 12:40:22 +0300

    remove comment

--- menu/models.py
+++ menu/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.urls import reverse, NoReverseMatch
 
-# Create your models here.
-
 class Menu(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
