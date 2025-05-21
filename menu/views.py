a2a0bce5c38feca990c6d386ee9ef124e6863b8c
Anastasia <anastasiya.e.kuzmenko@gmail.com>
2025-05-21 10:26:15 +0300

    add show_menu and home

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import MenuItem, Menu
 
 # Create your views here.
+
+def show_menu(request, path=None):
+    return render(request, 'main.html')
+
+def home(request):
+    return render(request, 'main.html')
