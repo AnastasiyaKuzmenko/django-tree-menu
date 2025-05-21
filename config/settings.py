6ac278bd599974c4d07fc23f06b2899437b3e601
Anastasia <anastasiya.e.kuzmenko@gmail.com>
2025-05-21 10:34:52 +0300

    add re_path

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -111,12 +112,15 @@ USE_I18N = True
 
 USE_TZ = True
 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver'] #test
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "menu", "static"),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
