3d41da3beac4409e7bf165af77a042ef639c61df
Anastasia <anastasiya.e.kuzmenko@gmail.com>
2025-05-21 10:36:59 +0300

    fix problem in get_absolute_url

--- menu/models.py
+++ menu/models.py
@@ -12,17 +12,24 @@ class Menu(models.Model):
 class MenuItem(models.Model):
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='items', null=True, blank=True)
     name = models.CharField('Name', max_length=100)
-    url = models.URLField(max_length=200, null=True, blank=True)
+    url = models.CharField(max_length=200, null=True, blank=True)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
     named_url = models.CharField(max_length=100, blank=True, help_text='Name of the URL pattern defined in urls.py')
 
     def get_absolute_url(self):
         if self.named_url:
             try:
-                return reverse(self.named_url)
+                url = reverse(self.named_url)
             except NoReverseMatch:
-                return '#'
-        return self.url or '#'
+                url = '#'
+        else:
+            url = self.url or '#'
+
+        if url != '#' and not url.startswith(('http://', 'https://', '/')):
+            url = '/' + url
+        if url != '#' and url.startswith('/') and not url.endswith('/'):
+            url = url + '/'
+        return url
 
     def __str__(self):
        return self.name
